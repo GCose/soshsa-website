24a2ed48ca4d68a4f962626d932209705a8e4f6c
GCose <goodnessadewuyi8@gmail.com>
2026-02-19 13:03:24 +0000

    Fixed bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+        revalidateIfStale: false,
+        dedupingInterval: 10000,
+        errorRetryCount: 3,
+        errorRetryInterval: 5000,
+        shouldRetryOnError: true,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
